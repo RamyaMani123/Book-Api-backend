e4945e457575559a734a75ef0335ef2b80d36ad8
Ramya <ramyamanikandan1986@gmail.com>
2026-06-01 19:39:15 +0530

    Add Dockerfile for Render deployment

--- obj/Debug/net9.0/BooksAndQuotesApplication.AssemblyInfo.cs
+++ obj/Debug/net9.0/BooksAndQuotesApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BooksAndQuotesApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a18bffa559f778459a5fb1b826f09631cb07203d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BooksAndQuotesApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BooksAndQuotesApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
